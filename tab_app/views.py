c2f4904a6ad4cf9bad5bea43c374fe7700b956c3
dhcrain <dhcrain@gmail.com>
2016-07-06 15:08:31 -0400

    can get song details on thier own page now

--- tab_app/views.py
+++ tab_app/views.py
@@ -4,14 +4,23 @@ from bs4 import BeautifulSoup
 # Create your views here.
 
 def tab_search_view(request):
-    url = "http://www.guitartabs.cc/search.php?tabtype=any&band=&song=fight+for+your+right"
+    song_name = request.GET.get('song_name')
+    url = "http://www.guitartabs.cc/search.php?tabtype=any&band=&song={}".format(song_name)
     content = requests.get(url).text
     souper = BeautifulSoup(content, 'html.parser')
     song_links = souper.find_all("a", class_="ryzh22")
-    band_name = souper.find(width="35%").text
+    band_name = souper.find(width="35%").text   # not working properly
     context = {
         'links': [link.attrs['href'] for link in song_links],
         'band_names': [str(band_name) for name in band_name], #not working currently
 
     }
     return render(request, 'index.html', context)
+
+def lyric_detail_view(request, url):
+    song = requests.get("http://www.guitartabs.cc/" + url).text
+    souper = BeautifulSoup(song, 'html.parser')
+    context = {
+        'tabs': str(souper.find(style="line-height:normal")),
+    }
+    return render(request, 'detail.html', context)
--- tabs_project/urls.py
+++ tabs_project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from tab_app.views import tab_search_view
+from tab_app.views import tab_search_view, lyric_detail_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', tab_search_view, name='tab_search_view'),
+    url(r'^(?P<url>.+)', lyric_detail_view, name="lyric_detail_view"),
+
 ]
