0c905daf058daefbbde7209b4431873c3aed164a
dhcrain <dhcrain@gmail.com>
2016-07-06 16:30:55 -0400

    making the index page more helpful

--- tab_app/views.py
+++ tab_app/views.py
@@ -4,16 +4,23 @@ from bs4 import BeautifulSoup
 # Create your views here.
 
 def tab_search_view(request):
+    page_count = 10
     song_name = request.GET.get('song_name')
-    url = "http://www.guitartabs.cc/search.php?tabtype=any&band=&song={}".format(song_name)
-    content = requests.get(url).text
-    souper = BeautifulSoup(content, 'html.parser')
-    song_links = souper.find_all("a", class_="ryzh22")
-    band_name = souper.find(width="35%").text   # not working properly
+    links = []
+    band_names = []
+    for page in range(1, page_count + 1):
+        url = "http://www.guitartabs.cc/search.php?tabtype=any&band=&song={}&p={}".format(song_name, page)
+        content = requests.get(url).text
+        souper = BeautifulSoup(content, 'html.parser')
+        song_links = souper.find_all("a", class_="ryzh22")
+        band_name = souper.find_all(width="35%")
+        for link in song_links:
+            links.append(link.attrs['href'])
+        for name in band_name:
+            band_names.append(str(name))
     context = {
-        'links': [link.attrs['href'] for link in song_links],
-        'band_names': [str(band_name) for name in band_name], #not working currently
-
+        'links': links,
+        'band_names': band_names,
     }
     return render(request, 'index.html', context)
 
