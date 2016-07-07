8824174748d63f8b327f594ce70ad965f6cbb07c
dhcrain <dhcrain@gmail.com>
2016-07-06 20:38:38 -0400

    looking at getting band pages to display well

--- tab_app/views.py
+++ tab_app/views.py
@@ -13,12 +13,12 @@ def tab_search_view(request):
         content = requests.get(url).text
         souper = BeautifulSoup(content, 'html.parser')
         song_links = souper.find_all("a", class_="ryzh22")
-        # song_links = souper.find(class_="tabslist fs-12")
         band_name = souper.find_all(width="35%")
         for link in song_links:
-            links.append(link.attrs['href'])
+            links.append(str(link))
         for name in band_name:
             band_names.append(str(name))
+        print(links)
     context = {
         'links': links,
         'band_names': band_names,
@@ -28,7 +28,16 @@ def tab_search_view(request):
 def lyric_detail_view(request, url):
     song = requests.get("http://www.guitartabs.cc/" + url).text
     souper = BeautifulSoup(song, 'html.parser')
-    context = {
-        'tabs': str(souper.find(style="line-height:normal")),
-    }
+    tabs = str(souper.find(style="line-height:normal"))
+    # print(tabs)
+    bands = []
+    for band in bands:
+        print(band)
+        band.append((souper.find_all("a", class_="ryzh2")))
+    print(bands)
+    if tabs == 'None':
+        # band = str(souper.find_all(class_='tabslist')[1])
+        context = {'songs': bands}
+    else:
+        context = {'tabs': tabs,}
     return render(request, 'detail.html', context)
